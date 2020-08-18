260f009b057e785fd0b47f068adb4fb7d39293b1
taevonjordan11 <taevonjordan1@gmail.com>
2020-08-18 15:54:49 -0400

    controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  def index
+    comments = comments.all
+     render json: comments
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+
+    comment.update!(comment_params)
+
+    render json: comment
+  end
+
+
+  def destroy
+    comment = Comment.find(params[:id])
+
+    comment.destroy!
+
+    render json: {}
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:park_id, :user_id, :description)
+  end
 end
--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,21 @@
 class ParksController < ApplicationController
+  def index
+    parks = Park.all
+
+    render json: parks
+  end
+
+  def update
+    park = Park.find(params[:id])
+
+    park.update!(park_params)
+
+    render json: park
+  end
+
+  private
+
+  def park_params
+    params.require(:park).permit(:name, :address, :contact, :likes, :image_url, :activities)
+  end
 end
